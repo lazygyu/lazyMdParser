069807fe0c1f87df0ad08a0384fd81ddd881ab19
Tag <tag@protopie.io>
2021-05-27 00:30:15 +0900

    add OL support

--- src/blocks/ListState.ts
+++ src/blocks/ListState.ts
@@ -4,7 +4,7 @@ import {IParser} from "../IParser";
 
 export class ListState implements IParser {
 	process(context: MdParser): MdBlockNode | false {
-		return this.processUl(context) || false;
+		return this.processUl(context) || this.processOl(context) || false;
 	}
 
 	makeLiItem(start, length) {
@@ -76,6 +76,62 @@ export class ListState implements IParser {
 		};
 	}
 
-	processOl(context) {
+	processOl(context): MdBlockNode | false {
+		let line = stripLineEnd(context.line);
+		let indent = 0;
+		const pat = /^[0-9]\./;
+		const start = context.cur;
+
+		const items = [];
+		let itemLines = [];
+
+		if (!line || !pat.test(line)) {
+			return false;
+		}
+
+		indent = 2;
+		itemLines.push(line.slice(indent));
+		let currentItem = this.makeLiItem(context.cur, 0);
+		context.goNextLine();
+
+		while (true) {
+			line = stripLineEnd(context.line);
+			if (isBlankLine(line) && !context.end) {
+				itemLines.push(line.slice(indent));
+				context.goNextLine();
+				continue;
+			}
+			if (line.startsWith('  ')) {
+				itemLines.push(line.slice(indent));
+				context.goNextLine();
+				continue;
+			}
+			if (pat.test(line)) {
+				currentItem.length = (context.cur) - currentItem.start;
+				currentItem.children = new MdParser().parse(itemLines.join('\n'));
+				itemLines = [];
+				items.push(currentItem);
+				currentItem = this.makeLiItem(context.cur, 0);
+				itemLines.push(line.slice(indent));
+				context.goNextLine();
+				continue;
+			}
+
+			currentItem.length = (context.cur) - currentItem.start;
+			currentItem.children = new MdParser().parse(itemLines.join('\n'));
+			items.push(currentItem);
+			itemLines = [];
+			break;
+		}
+
+		return {
+			type: 'containerBlock',
+			name: 'orderedList',
+			tag: 'ol',
+			start,
+			allowInlines: false,
+			length: context.cur - start,
+			children: items
+		};
 	}
 }
